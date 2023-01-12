b35b9e783bd5fed901018620010d4cdbebfb2516
Emmanuel <emmanuel.ruaud.ext@cosmotech.com>
2023-01-12 17:25:44 +0100

    empty react app with env vriable

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>{this.state.counter}</h2>
+        <h3>{process.env.REACT_APP_DATA}</h3>
+        <button onClick={
+          () => this.setState({ counter: (this.state.counter + 1) })
+        }>increase</button>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
