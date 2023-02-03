70ecb3300988219e9698855158bbf37e050397af
Emmanuel Ruaud <em.ruaud@gmail.com>
2023-02-03 10:00:33 +0100

    update flow

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ class App extends React.Component {
         <h2>{this.state.counter}</h2>
         <h3>{process.env.REACT_APP_DATA}</h3>
         <span>{process.env.REACT_APP_ORGANIZATION_ID}</span>
+        <span>{process.env.COSMOTECH_API_SCOPE}</span>
         <button onClick={
           () => this.setState({ counter: (this.state.counter + 1) })
         }>increase</button>
